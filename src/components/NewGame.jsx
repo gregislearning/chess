5ea2a5f9a0d89a2b374551705350fde2ac8f6879
Greg <greg@ekohe.com>
2021-07-15 09:46:40 +0800

    moved bpiece move state to board

---
+++ src/components/NewGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const NewGame = props => {
+  const handleClick = () => {
+
+  }
+  return <Button onClick={handleClick}>New Game</Button>
+}
+
+export default NewGame
--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,10 @@
 import { init } from '@rematch/core'
 import positions from './positions'
+import test from './test'
 const store = init({
   models: {
     positions,
+    test
   }
 })
 
